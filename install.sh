548044f5f1d0d034eb4ff74bdb13a68bf5255234
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-05-31 12:15:06 +0100

    Update installer

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ else
     # Make sure we're running the latest
     git --git-dir ~/.remote-shell/.git --work-tree ~/.remote-shell pull origin master
     # Submodule update doesn't support --work-tree, so cd in there using a subshell
-    (cd ~/.remote-shell && git submodule update --remote)
+    (cd ~/.remote-shell && git submodule update)
 fi
 
-# Prompt for name. 
+# Prompt for name.
 name=
 while [[ ! $name ]]; do
     echo "What's your name? This will be used to create your custom profile"
@@ -18,7 +18,7 @@ while [[ ! $name ]]; do
     read name
 done
 
-# If the custom bashrc doesn't exist, create it. 
+# If the custom bashrc doesn't exist, create it.
 # The default template is to source the common profile
 # as well as any custom profile that's checked in.
 if [[ ! -f "${HOME}/.bashrc_$name" ]]; then
@@ -35,30 +35,30 @@ source \"${template_common}\"
 
 " >> "${HOME}/.bashrc_$name"
 else
-    echo ".bashrc_$name already exists. Skipping."    
+    echo ".bashrc_$name already exists. Skipping."
 fi
 
-# Create git customizations if they don't exist. 
-if [[ ! -f "${HOME}/.gitconfig_$name" ]]; then 
+# Create git customizations if they don't exist.
+if [[ ! -f "${HOME}/.gitconfig_$name" ]]; then
     echo "Creating .gitconfig_$name"
     echo "# Place your custom git config in here" > "${HOME}/.gitconfig_$name"
 fi
 
 # Ask the user for their IP.
 # If provided, inject it into the bashrc along with the call
-# to source the custom bashrc. 
+# to source the custom bashrc.
 echo "Enter your static IP. Leave blank if you don't have one."
 read ip
 
 if [[ $ip ]]; then
-    # If ~/.bashrc doesn't exist, create it. 
+    # If ~/.bashrc doesn't exist, create it.
     if [ ! -f "${HOME}/.bashrc" ]; then
         echo 'Creating bashrc.'
         echo > ~/.bashrc
     fi
 
     # Check if the IP exists in the bashrc file.
-    if grep -q $ip ~/.bashrc; then 
+    if grep -q $ip ~/.bashrc; then
         echo 'This IP has already been loaded.'
     else
         echo "

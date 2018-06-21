5b2dff91be59b1f57ac572926d0a8a45ee6f8ecb
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-06-21 20:45:12 +0100

    Update drush launcher

--- DrupalFinder.php
+++ DrupalFinder.php
@@ -1,44 +1,66 @@
 <?php
 
-Class Drupal {
-  public function findRoot($start_path) {
-    $path = $start_path;
+function findRoot($start_path) {
+  $path = $start_path;
 
-    # Traverse up the directory structure until we find
-    # a valid drupal root, or we hit the system root.
-    do {
-      $valid = $this->isValidPath($path);
+  # Traverse up the directory structure until we find
+  # a valid drupal root, or we hit the system root.
+  do {
+    $valid = isValidPath($path);
 
-      if ($valid) {
-        return $path;
-      }
-
-      $path = dirname($path);
-    } while($path);
-
-    return $null;
-  }
-
-  private function isValidPath($path) {
-    $valid = file_exists("$path/web") && file_exists("$path/composer.json") && file_exists("$path/vendor/drush");
-    return $valid;
-  }
-
-  public function findDrush($root) {
-    if ($root) {
-      if(file_exists("$root/vendor/drush/drush/drush")) {
-        return "$root/vendor/drush/drush/drush";
-      }
+    if ($valid) {
+      return $path;
     }
-    return null;
-  }
 
-  public function findDrupalConsole($root) {
-    if ($root) {
-      if(file_exists("$root/vendor/drupal/console/bin/drupal")) {
-        return "$root/vendor/drupal/console/bin/drupal";
-      }
+    $path = dirname($path);
+  } while($path);
+
+  return $null;
+}
+
+function isValidPath($path) {
+  $valid = file_exists("$path/web") && file_exists("$path/composer.json") && file_exists("$path/vendor/drush");
+  return $valid;
+}
+
+function findDrush($root) {
+  if ($root) {
+    if(file_exists("$root/vendor/drush/drush/drush")) {
+      return "$root/vendor/drush/drush/drush";
     }
-    return null;
   }
+  return null;
+}
+
+function findDrupalConsole($root) {
+  if ($root) {
+    if(file_exists("$root/vendor/drupal/console/bin/drupal")) {
+      return "$root/vendor/drupal/console/bin/drupal";
+    }
+  }
+  return null;
+}
+
+if (!isset($argv[1])) {
+  echo "Unknown command. Specify 'root', 'drush' or 'drupal-console'";
+  exit(1);
+}
+
+$path = getcwd();
+$command = $argv[1];
+
+if(isset($argv[2])) {
+  $path = $argv[2];
+}
+
+switch($command) {
+  case 'root':
+    echo findRoot($path);
+    break;
+  case 'drush':
+    echo findDrush(findRoot($path));
+    break;
+  case 'drupal-console':
+    echo findDrupalConsole(findRoot($path)); 
+    break;
 }
